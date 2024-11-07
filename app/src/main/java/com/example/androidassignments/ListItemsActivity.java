8f9ce723918045ed6027a799b66db2b6143b3812
Gurparkash-Singh <singhgk.fateh@gmail.com>
2024-11-06 19:42:29 -0500

    Added Assignment 2 Part 3 and 4

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -1,191 +1,184 @@
 package com.example.androidassignments;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.Manifest;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ListItemsActivity extends AppCompatActivity {
 
-    private static final String activityName = "ListItemsActivity";
-    private ImageButton imageButton;
-    private Switch switchButton;
-    private CheckBox checkbox;
+    private static final int REQUEST_IMAGE_CAPTURE =1;
+    private static final int REQUEST_CAMERA_PERMISSION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        EdgeToEdge.enable(this);
+        Log.i("ListItemsActivity", "onCreate called");
         setContentView(R.layout.activity_list_items);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        String functionName = "onCreate";
-        Log.i(activityName, functionName + " in " + activityName);
-
-        // Request camera permissions
-        if (ContextCompat.checkSelfPermission(
-                ListItemsActivity.this, "android.permission.CAMERA") !=
-                PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(ListItemsActivity.this, new String[]{
-                    "android.permission.CAMERA"
-            }, 1);
-        }
-
-        // Get the UI elements
-        imageButton = (ImageButton) findViewById(R.id.imageButton);
-        switchButton = (Switch) findViewById(R.id.switch1);
-        checkbox = (CheckBox) findViewById(R.id.checkBox);
-
+        // Button to capture image using the camera
+        ImageButton imageButton = findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                try {
-                    startActivityForResult(takePictureIntent, 1);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(ListItemsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(ListItemsActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListItemsActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } else {
+                    dispatchTakePictureIntent();
                 }
             }
         });
 
-        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        // Button to go back to MainActivity
+        Button mainButton = findViewById(R.id.buttonMain);
+        mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                String switchOn = getResources().getString(R.string.switchOn);
-                String switchOff = getResources().getString(R.string.switchOff);
-                if (b)
-                {
-                    Toast.makeText(ListItemsActivity.this, switchOn, Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(ListItemsActivity.this, switchOff, Toast.LENGTH_LONG).show();
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(ListItemsActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
-        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        Button logoutButton = findViewById(R.id.buttonLogout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                AlertDialog.Builder dialog = new AlertDialog.Builder(ListItemsActivity.this);
-                dialog.setTitle(getResources().getString(R.string.yes));
-                dialog.setMessage(getResources().getString(R.string.exitMessage));
-                dialog.setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent resultIntent = new Intent(  );
-                        resultIntent.putExtra("Response", getResources().getString(R.string.response));
-                        setResult(Activity.RESULT_OK, resultIntent);
-                        finish();
-                    }
-                });
-                dialog.setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.i(activityName, "Staying here");
-                    }
-                });
-                dialog.create();
-                dialog.show();
+            public void onClick(View v) {
+                Intent intent = new Intent(ListItemsActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Switch to show a toast message
+        Switch mySwitch = findViewById(R.id.switch2);
+        mySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            String message = isChecked ? "Switch is On" : "Switch is Off";
+            int duration = isChecked ? Toast.LENGTH_SHORT : Toast.LENGTH_LONG;
+            Toast.makeText(ListItemsActivity.this, message, duration).show();
+        });
+
+        // Checkbox to show a dialog box
+        CheckBox myCheckBox = findViewById(R.id.checkBox2);
+        myCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                builder.setMessage(R.string.button_dialog_message)
+                        .setTitle(R.string.button_dialog_title)
+                        .setPositiveButton(R.string.button_ok, (dialog, id) -> {
+                            Intent resultIntent = new Intent();
+                            resultIntent.putExtra("Response", "Here is my response");
+                            setResult(Activity.RESULT_OK, resultIntent);
+                            finish();
+                        })
+                        .setNegativeButton(R.string.button_cancel, (dialog, id) -> dialog.dismiss())
+                        .show();
             }
         });
     }
 
-    private void print(String message)
-    {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (Exception e) {
+                Log.e("CameraError", "Error starting camera activity: " + e.getMessage());
+            }
+        } else {
+            Toast.makeText(this, "No camera app available", Toast.LENGTH_SHORT).show();
+            Log.e("CameraError", "No app available to handle camera intent");
+        }
+    }
+
+    // Handle the result of the camera activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                ImageView imageView = findViewById(R.id.capturedImageView);
+                if (imageBitmap != null) {
+                    imageView.setImageBitmap(imageBitmap);
+                } else {
+                    Toast.makeText(this, "Failed to capture image", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "No image captured", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1)
-        {
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            imageButton.setImageBitmap(bitmap);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with camera intent
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Camera permission is required to take pictures", Toast.LENGTH_SHORT).show();
+                Log.e("PermissionError", "Camera permission denied");
+            }
         }
+    }
+
+    // Lifecycle methods for logging
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ListItemsActivity", "onStart called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        String functionName = "onResume";
-        Log.i(activityName, functionName + " in " + activityName);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        String functionName = "onStart";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("ListItemsActivity", "onResume called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        String functionName = "onPause";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("ListItemsActivity", "onPause called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
-        String functionName = "onStop";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("ListItemsActivity", "onStop called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        String functionName = "onDestroy";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("ListItemsActivity", "onDestroy called");
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        String functionName = "onSaveInstanceState";
-        Log.i(activityName, functionName + " in " + activityName);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("ListItemsActivity", "onSaveInstanceState called");
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
-        String functionName = "onRestoreInstanceState";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("ListItemsActivity", "onRestoreInstanceState called");
     }
 }
--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -1,165 +1,121 @@
 package com.example.androidassignments;
 
+import static java.lang.System.exit;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.text.TextUtils;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private static final String activityName = "LoginActivity";
-    private static final String preferencesFile = "preferences";
-    private static final String preferenceEmailField = "defaultEmail";
-    private Button loginButton;
-    private EditText emailField;
-    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        Log.i("LoginActivity", "onCreate called");
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button testToolbarButton = findViewById(R.id.buttonTestToolbar);
+        testToolbarButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, TestToolbar.class);
+            startActivity(intent);
         });
 
-        String functionName = "onCreate";
-        Log.i(activityName, functionName + " in " + activityName);
+        // Fetch the last used email from SharedPreferences
+        SharedPreferences prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        String savedEmail = prefs.getString("DefaultEmail", "");  // Fetch the saved email (default is empty)
+        EditText emailField = findViewById(R.id.LoginEmail);  // Assuming ID is LoginEmail
+        EditText passwordField = findViewById(R.id.passwordEditText); // Assuming this is the ID for the password field
 
-        // Get the Email Input and Login Button
-        loginButton = (Button) findViewById(R.id.loginButton);
-        emailField = (EditText) findViewById(R.id.emailField);
-        passwordField = (EditText) findViewById(R.id.passwordField);
+        emailField.setText(savedEmail);  // Set the saved email to the email field
 
-        // Get the default email from shared preferences
-        String defaultEmail = loadPreferences();
-
-        // set email field to default email
-        emailField.setText(defaultEmail);
-
-        // Add an event listener to the login button
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        // Button to go back to MainActivity
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                // Get Email and Password from user
-                String email = emailField.getText().toString();
-                String password = passwordField.getText().toString();
+                String email = emailField.getText().toString();  // Get the email from the EditText field
+                String password = passwordField.getText().toString().trim();
 
-                // Validate Email and Password
-                if (TextUtils.isEmpty(email))
-                {
-                    createDialog(getResources().getString(R.string.emptyEmail));
+                if(password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (!email.isEmpty()) {  // Check if email is not empty
+                    // Save the entered email in SharedPreferences
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("DefaultEmail", email);  // Save the email in SharedPreferences
+                    editor.apply();  // Apply changes to SharedPreferences
 
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-                {
-                    createDialog(getResources().getString(R.string.invalidEmail));
-                    return;
+                    // Log for debugging
+                    Log.i("LoginActivity", "Email saved: " + email);
+
+                    // Start MainActivity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // If email is empty, show a Toast message
+                    Toast.makeText(LoginActivity.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
                 }
-
-                if (TextUtils.isEmpty(password))
-                {
-                    createDialog(getResources().getString(R.string.emptyPassword));
-                    return;
-                }
-
-                // Save email to shared Preferences
-                savePreferences(email);
-
-                // Show Main Activity
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
             }
         });
-    }
 
-    private String loadPreferences()
-    {
-        SharedPreferences preferences = getSharedPreferences(preferencesFile, MODE_PRIVATE);
-        return preferences.getString(preferenceEmailField, "email@domain.com");
-    }
-
-    private void savePreferences(String defaultEmail)
-    {
-        SharedPreferences preferences = getSharedPreferences(preferencesFile, MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(preferenceEmailField, defaultEmail);
-        editor.apply();
-    }
-
-    private void createDialog(String message)
-    {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        String functionName = "onResume";
-        Log.i(activityName, functionName + " in " + activityName);
+        // Button to go to ListItemsActivity
+        //Button listItemsButton = findViewById(R.id.buttonListItems);
+        //listItemsButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent = new Intent(LoginActivity.this, ListItemsActivity.class);
+        //        startActivity(intent);
+        //    }
+        //});
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i("LoginActivity", "onStart called");
+    }
 
-        String functionName = "onStart";
-        Log.i(activityName, functionName + " in " + activityName);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("LoginActivity", "onResume called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        String functionName = "onPause";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("LoginActivity", "onPause called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
-        String functionName = "onStop";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("LoginActivity", "onStop called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        String functionName = "onDestroy";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("LoginActivity", "onDestroy called");
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        String functionName = "onSaveInstanceState";
-        Log.i(activityName, functionName + " in " + activityName);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("LoginActivity", "onSaveInstanceState called");
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
-        String functionName = "onRestoreInstanceState";
-        Log.i(activityName, functionName + " in " + activityName);
+        Log.i("LoginActivity", "onRestoreInstanceState called");
     }
 }
